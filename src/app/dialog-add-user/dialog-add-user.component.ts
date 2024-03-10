3a82d825776a09b5b317c3f17bbd8e2591555dc5
Daxi90 <hdateam90@gmail.com>
2024-03-10 10:24:34 +0100

    added user model

---
+++ src/app/dialog-add-user/dialog-add-user.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatInputModule } from '@angular/material/input';
+import { provideNativeDateAdapter } from '@angular/material/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+
+@Component({
+  selector: 'app-dialog-add-user',
+  standalone: true,
+  imports: [
+    MatButtonModule,
+    MatDividerModule,
+    MatIconModule,
+    MatTooltipModule,
+    MatDialogModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+  ],
+  providers: [provideNativeDateAdapter()],
+  templateUrl: './dialog-add-user.component.html',
+  styleUrls: ['./dialog-add-user.component.scss'],
+})
+export class DialogAddUserComponent {
+  // Your component logic
+}
